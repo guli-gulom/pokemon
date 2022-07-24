40f21f53b3065ac2d68e18377010b4bb32d46f31
guli-gulom <guli.gulom1254@gmail.com>
2022-07-24 08:46:33 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,217 @@
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement)
+const logoImg = "./images/pokemon_logo 1.png"
+const cardImg = "./images/004 1.png"
+
+const websiteLogo = React.createElement("div",{className: "logo"}, [
+    React.createElement("img", {
+        src: logoImg,
+        alt: "pokemon",
+        className: "logo-image",
+    }),
+    React.createElement("i",{className: "far fa-heart"}),
+])
+
+const btnGroup = React.createElement("div", {className: "btn-group"},
+ [
+React.createElement("button", {className: "btn"}, "Gross", React.createElement("i", {className:"fas fa-angle-down",})),
+React.createElement("button", {className: "btn"}, "Search", React.createElement("i", {className:"fas fa-search",})),
+React.createElement("button", {className: "btn"}, "Aa-Zz", React.createElement("i", {className:"fas fa-angle-down",})),
+React.createElement("button", {className: "btn"}, "Button",),
+
+])
+
+const cardGroup = React.createElement("div", {className: "card-group"}, [
+  ,
+  React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+]),
+React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+]),
+React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+]),
+React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+]),
+React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+]),
+React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+]),
+React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+]),
+React.createElement("div", {className: "card"}, [
+    React.createElement("div", {className: "card-header"}, [
+        React.createElement("img", {
+           className: "cardImage" ,
+           src: cardImg,
+           alt: "pokemon",
+        })
+    ]),
+    React.createElement("div", {className: "card-body"}, [
+      React.createElement("div", {className: "for-text"}, [
+        React.createElement("div", {className: "cardText" }, [
+            React.createElement("h3", {className: "title"}, "Pokesmon"),
+            React.createElement("p", {className: "desc1"}, "Grass, Poison"),
+        ]),
+        React.createElement("i",{className: "far fa-heart"}),
+      ]),
+      React.createElement("div", {className: "product-weight"}, [
+        React.createElement("p", {className: "desc"}, "6.9 kg"),
+        React.createElement("p", {className: "desc"}, "99 age")
+      ])
+    ])
+
+])
+] )
+
+const container = React.createElement("div", {className: "container"}, [
+   websiteLogo,
+   btnGroup,
+   cardGroup, 
+])
+root.render(container)
